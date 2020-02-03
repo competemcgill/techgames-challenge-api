a0e020c49033326958fcdf83de82f1378942070b
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2020-02-02 19:40:50 -0500

    Added validators for user routes

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,30 +1,48 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator/check";
+import { errorMessage } from "../util/errorFormatter";
+import { statusCodes } from "../util/statusCodes";
 
 const userController = {
 
     index: async (req: Request, res: Response) => {
-        // TODO
-        res.status(200).send({msg: "Not implemented"});
+        res.status(statusCodes.SUCCESS).send({msg: "Not implemented"});
     },
 
     show: async (req: Request, res: Response) => {
-        // TODO
-        res.status(200).send({msg: "Not implemented"});
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(statusCodes.MISSING_PARAMS).json(errors.formatWith(errorMessage).array()[0]);
+        } else {
+            res.status(statusCodes.SUCCESS).send({msg: "Not implemented"});
+        }
     },
 
     create: async (req: Request, res: Response) => {
-        // TODO
-        res.status(200).send({msg: "Not implemented"});
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(statusCodes.MISSING_PARAMS).json(errors.formatWith(errorMessage).array()[0]);
+        } else {
+            res.status(statusCodes.SUCCESS).send({msg: "Not implemented"});
+        }
     },
 
     update: async (req: Request, res: Response) => {
-        // TODO
-        res.status(200).send({msg: "Not implemented"});
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(statusCodes.MISSING_PARAMS).json(errors.formatWith(errorMessage).array()[0]);
+        } else {
+            res.status(statusCodes.SUCCESS).send({msg: "Not implemented"});
+        }
     },
 
     delete: async (req: Request, res: Response) => {
-        // TODO
-        res.status(200).send({msg: "Not implemented"});
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(statusCodes.MISSING_PARAMS).json(errors.formatWith(errorMessage).array()[0]);
+        } else {
+            res.status(statusCodes.SUCCESS).send({msg: "Not implemented"});
+        }
     }
 };
 
---
+++ src/util/statusCodes.ts
@@ -0,0 +1,6 @@
+export const statusCodes = {
+    SUCCESS: 200,
+    NOT_FOUND: 404,
+    MISSING_PARAMS: 422,
+    SERVER_ERROR: 500
+}
