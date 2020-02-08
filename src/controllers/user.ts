537fc7da7e94bccc2690fbf2b260d8a57f16aefc
Hassan Haidar <hassan.haidar@mail.mcgill.ca>
2020-02-08 15:29:59 -0500

    implement Show User controller :sparkles:

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { validationResult } from "express-validator/check";
 import { errorMessage } from "../util/errorFormatter";
 import { statusCodes } from "../util/statusCodes";
+import { User, IUserModel } from "../database/models/user";
+import { userDBInteractions} from "../database/interactions/user";
 
 const userController = {
 
@@ -13,8 +15,16 @@ const userController = {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             res.status(statusCodes.MISSING_PARAMS).json(errors.formatWith(errorMessage).array()[0]);
-        } else {
-            res.status(statusCodes.SUCCESS).send({msg: "Not implemented"});
+        }
+        else {
+            try {
+                const userId: string = req.params.userId;
+                const user: IUserModel = await userDBInteractions.find(userId);
+                user? res.status(statusCodes.SUCCESS).send(user) : res.status(statusCodes.NOT_FOUND).send({status: 404, message: "User not found" });
+            }
+            catch(error){
+                res.status(500).send(error);
+            }
         }
     },
 
